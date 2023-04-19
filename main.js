dde7d4cac6ea70f72478d90a6f4b180d73ca5b68
Kallurisravani12 <KalluriSravani12>
2023-04-19 05:05:35 +0200

    My first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    var c=a+b;
+    console.log(c);
+
+
+}
+add(3,5);
+add(3,15);
+
+
+
+function add(a,b){
+    var c=a+b;
+    return c;
+}
+var result1 = add(3,9); 
+console.log(result1);
+
+var result2 = add(6,9);
+console.log(result2);
